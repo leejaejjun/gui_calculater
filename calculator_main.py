a45e4e6c5a7721ac21e7de52f081ee91d0c5ebbb
leejaejjun <77061306+leejaejjun@users.noreply.github.com>
2022-11-25 16:05:11 +0900

    number array change

--- calculator_main.py
+++ calculator_main.py
@@ -90,7 +90,7 @@ class Main(QDialog):
                                                        self.number_button_clicked(num))
             if number >0:
                 x,y = divmod(number-1, 3)
-                layout_number.addWidget(number_button_dict[number], x, y)
+                layout_number.addWidget(number_button_dict[number], 2-x, y)
             elif number==0:
                 layout_number.addWidget(number_button_dict[number], 3, 1)
 
